6431630ddce8e33664b889b46eea66eb4b400b3b
johndavid400 <jd@isotope11.com>
2012-05-04 14:01:17 -0500

    as

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+require 'bundler/capistrano'
+
+set :application, "staging.prototyperobotics.com"
+role :web, "staging.prototyperobotics.com"                          # Your HTTP server, Apache/etc
+role :app, "staging.prototyperobotics.com"                          # This may be the same as your `Web` server
+role :db,  "staging.prototyperobotics.com", :primary => true # This is where Rails migrations will run
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/home/deployer/staging"
+set :user, "deployer"
+set :use_sudo, false
+
+# repo details
+set :scm, :git
+#set :git_username, "johndavid400"
+set :repository, "git@github.com:johndavid400/prototype_robotics.git"
+set :branch, "staging"
+set :git_enable_submodules, 1
+
+# runtime dependencies
+depend :remote, :gem, "bundler"
+
+# tasks
+namespace :deploy do
+  before 'deploy:setup', :db
+
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared resources on each release"
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+#    run "ln -nfs #{shared_path}/ckeditor_assets #{release_path}/public/ckeditor_assets"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'c3172eed31ab8aad182a5b5288597a92ab6f05fbbcbcf532a87c0cff3bf2ea5d6297f1e83e59a7e1550bec8831fbb2f0899238a2671fc42e531dea010082c890'
